05e8e25dea0d659109cbea92ef8a5188c9558d29
marcelsev <marcelosolanosev@gmail.com>
2022-09-23 13:54:25 +0200

    backend find user

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,13 +1,28 @@
-const UserModel = require ("../models/user");
-
+const UserModel = require("../models/user");
+const ObjectID = require('mongoose').Types.ObjectId;
 
 module.exports.signup = async (req, res, next) => {
-    const {pseudo, email, password } =  req.body
+    const { pseudo, email, password } = req.body
     try {
-        const user = await UserModel.create({ pseudo, email, password});
-        res.status(201).json ({user: user._id});
+        const user = await UserModel.create({ pseudo, email, password });
+        res.status(201).json({ user: user._id });
     }
-    catch (err){
-        res.status(200).send({err})
+    catch (err) {
+        res.status(200).send({ err })
     }
 }
+
+module.exports.getAllUsers = async (req, res, next) => {
+    const users = await UserModel.find().select('-password');
+    res.status(200).json(users);
+}
+
+module.exports.userInfo = (req, res, next) => {
+    if (!ObjectID.isValid(req.params.id))
+        return res.status(400).send('ID unknown : ' + req.params.id)
+
+    UserModel.findById(req.params.id, (err, docs) => {
+        if (!err) res.send(docs);
+        else console.log('ID unknown : ' + err);
+    }).select('-password');
+};
