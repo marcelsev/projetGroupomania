4968c438bedfdc96e481de1e70ea7bd41aa885af
marcelsev <marcelosolanosev@gmail.com>
2022-10-04 20:31:41 +0200

    post backend et frontend

--- frontend/src/components/postform.js
+++ frontend/src/components/postform.js
@@ -1,47 +1,50 @@
 import React, { useEffect, useState } from 'react';
-import { isEmpty, timestampParser } from './utils';
+import { timestampParser } from './utils';
 import axios from 'axios';
 
 
 const Postform = () => {
-    const [isLoading, setIsLoading] = useState(true);
+    // const [isLoading, setIsLoading] = useState(true);
     const [message, setMessage] = useState("");
-    const [postPicture, setPostPicture] = useState(null);
+    //const [postPicture, setPostPicture] = useState(null);
     const [video, setVideo] = useState('');
-    const [file, setFile] = useState();
+    const [file, setFile] = useState(null);
 
     const handlePicture = (e) => {
-        setPostPicture(URL.createObjectURL(e.target.files[0]));
+        setFile(URL.createObjectURL(e.target.files[0]));
         setVideo("");
-        setFile(e.target.files[0]);
     }
 
-    const handlePost = (e) => {
-        e.preventDefault();
-
-        axios({
-            method: "POST",
-            url: `http://localhost:3000/api/post/feed`,
-            data: {
-                postPicture,
+    const handlePost = () => {
+        if (message || file || video) {
+            const data = {
                 message,
-                video, 
+                video,
                 file
             }
-        })
-            .then((res) => {
-                window.location = `/feed`;
-            })
-            
-        .catch ((err)=> {
-            console.log (err)
-        })
-    };
+            const option = {
+                headers:
+            {Authorization : 'Bearer ' + JSON.parse(localStorage.getItem('token'))}}
+            axios.post( 'http://localhost:3000/api/post/feed',
+                data,
+                option)
+    
+                .then((res) => {
+                    if (res.data){
+                        window.location = '/feed'
+                    } else{}
 
+
+                })
+                .catch((error) => { console.log(error, 'error big') })
+        } else {
+            alert("mettre text ")
+        }
+    }
+   
 
     const cancelPost = () => {
         setMessage('');
-        setPostPicture('');
         setVideo('');
         setFile('');
     };
@@ -49,7 +52,6 @@ const Postform = () => {
 
 
     useEffect(() => {
-        if (!isEmpty()) setIsLoading(false);
 
         const handleVideo = () => {
             let findLink = message.split(" ");
@@ -62,7 +64,7 @@ const Postform = () => {
                     setVideo(embed.split("&")[0]);
                     findLink.splice(i, 1);
                     setMessage(findLink.join(" "));
-                    setPostPicture('');
+                    setFile('');
                 }
             }
         };
@@ -71,35 +73,35 @@ const Postform = () => {
 
     return (
         <div className='container-form-post'>
-           
-                    <div className='post-form'>
-                        <textarea name='message' id='message' placeholder='Quoi de neuf?' onChange={(e) => setMessage(e.target.value)} value={message} />
-                        <div className='content-container'>
-                            <div className="content">
-                                <p>{message}</p>
-                                <img src={postPicture} alt="" />
-                                {video && (
-                                    <iframe
-                                        src={video}
-                                        frameBorder="0"
-                                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                        allowFullScreen
-                                        title={video}
-                                    ></iframe>
-                                )}
-                            </div>
-                            <div className='footer-form'>
-                                <>  <input type="file" id='file-upload' name='file' accept='.jpg, .jpeg, .png' onChange={(e) => handlePicture(e)} />
-                                </>
-                                <br />
-                                <span>{timestampParser(Date.now())}</span>
-                            </div>
-                            <div className='btn-send'>
-                                <button className='cancel' onClick={cancelPost}>Annuler message</button>
-                                <button className='send' onClick={handlePost}>Envoyer</button>
-                            </div>
-                        </div>
+
+            <div className='post-form'>
+                <textarea name='message' id='message' placeholder='Quoi de neuf?' onChange={(e) => setMessage(e.target.value)} value={message} />
+                <div className='content-container'>
+                    <div className="content">
+                        <p>{message}</p>
+                        <img src={file} alt="" />
+                        {video && (
+                            <iframe
+                                src={video}
+                                frameBorder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                allowFullScreen
+                                title={video}
+                            ></iframe>
+                        )}
                     </div>
+                    <div className='footer-form'>
+                        <>  <input type="file" id='file-upload' name='file' accept='.jpg, .jpeg, .png' onChange={(e) => handlePicture(e)} />
+                        </>
+                        <br />
+                        <span>{timestampParser(Date.now())}</span>
+                    </div>
+                    <div className='btn-send'>
+                        <button className='cancel' onClick={cancelPost}>Annuler message</button>
+                        <button className='send' onClick={handlePost}>Envoyer</button>
+                    </div>
+                </div>
+            </div>
         </div>
 
     );
